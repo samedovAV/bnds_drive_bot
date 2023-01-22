119cee98d8606671611050780539d5e4f3cd30ba
Samedov Aleksandr <alekcandr206@gmail.com>
2023-01-22 23:24:24 +0100

    init

---
+++ src/main/java/com/samedov/googledrivebot/GoogleDriveBotApplication.java
@@ -0,0 +1,13 @@
+package com.samedov.googledrivebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleDriveBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samedov/googledrivebot/GoogleDriveBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samedov.googledrivebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDriveBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
